fb6861a429d5516543d0e4c21f031497cf81a162
fanmu95 <550072252@qq.com>
2022-12-14 19:12:06 +0800

    罗技lua

---
+++ 狙击.lua
@@ -0,0 +1,49 @@
+ClearLog()
+local juji = false
+local usb = true
+
+EnablePrimaryMouseButtonEvents(true)
+ 
+function OnEvent(event, arg)
+  if (event == "MOUSE_BUTTON_PRESSED" and arg == 4) then
+    usb  = not usb 
+    juji = not juji
+       PressKey("lctrl") 
+       Sleep(100)
+       PressKey("z")
+       Sleep(100) 
+       ReleaseKey("lctrl")
+       ReleaseKey("z")
+  end
+  
+  if (event == "MOUSE_BUTTON_PRESSED" and arg == 2) then
+       if juji then 
+       PressAndReleaseKey("equal")
+       end
+   end
+   
+  Sleep(1)
+  if usb then
+    while IsMouseButtonPressed(1) do
+      PressKey("minus") 
+      Sleep(math.random(30, 50))
+      ReleaseKey("minus")
+      Sleep(math.random(20, 35))
+    end
+  end
+  
+   if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
+   if juji then
+       ReleaseKey("equal")
+     Sleep(2)
+       PressAndReleaseKey("minus")
+       Sleep(2)
+       MoveMouseWheel(-1)
+       Sleep(math.random(140,150))
+       MoveMouseWheel(1)
+        Sleep(30)
+       PressAndReleaseKey("1")
+       end
+   end
+  
+end
