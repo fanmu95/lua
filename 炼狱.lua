a170762c16927df25d9e985ca27637ea99049783
fanmu <49608629+fanmu95@users.noreply.github.com>
2024-01-25 16:03:37 +0800

    Update 炼狱.lua

--- 炼狱.lua
+++ 炼狱.lua
@@ -1,9 +1,9 @@
 ClearLog()
 local lianyu = true
 EnablePrimaryMouseButtonEvents(true)
-function OnEvent(event, arg)
+
  
-  Sleep(1)
+function OnEvent(event, arg)
   if lianyu then
     while IsMouseButtonPressed(1) do
       PressKey("minus") 
@@ -13,13 +13,36 @@ function OnEvent(event, arg)
     end
   end
   
-    if lianyu  then
+  if lianyu  then
     while IsMouseButtonPressed(3) do
      PressAndReleaseKey("equal")
      Sleep(math.random(270, 300))
      PressAndReleaseKey("minus")
      Sleep(math.random(15, 30))
      PressAndReleaseKey("R")
+    end
+  end
+
+  if lianyu  then
+    while IsMouseButtonPressed(5) do
+      PressKey("spacebar")
+      Sleep(25)
+      ReleaseKey("spacebar")
+      Sleep(595)
+      PressKey("spacebar")
+      Sleep(25)
+      ReleaseKey("spacebar")
+      Sleep(125)
+      PressKey("lctrl")
+      Sleep(125)
+      ReleaseKey("lctrl")
+    end
+  end
+
+  if lianyu  then
+    while IsMouseButtonPressed(4) do
+      PressAndReleaseKey("lctrl")
+      Sleep(math.random(100, 150))
     end
   end
 end
